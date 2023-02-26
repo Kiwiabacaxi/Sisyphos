9501f9641b4aa8f13a5a3061c2f4e7631a11461e
Carlos Alexandre <101848698+Kiwiabacaxi@users.noreply.github.com>
2023-02-26 01:22:51 -0300

    Delete Beecrowd/Whiskey in the Jar directory

--- Beecrowd/Whiskey in the Jar/Soma_Simples.py
+++
@@ -1,8 +0,0 @@
-""" Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável. """
-
-A = int(input())
-B = int(input())
-SOMA = A + B
-print("SOMA = %d" % SOMA)
-
-# print(f"SOMA = {int(input()) + int(input())})
